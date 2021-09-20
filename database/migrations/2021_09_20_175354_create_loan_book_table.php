89fcdf9185a8727f4f4cac883ad9a95a97e00ec8
alrom2525 <edgardoalbertoromero@gmail.com>
2021-09-20 14:14:48 -0400

    migrate

---
+++ database/migrations/2021_09_20_175354_create_loan_book_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoanBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_book', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id', 'fk_loan_book_user')->references('id')->on('users')->onDelete('restrict')->onUpdate('restrict');
+            $table->unsignedInteger('book_id');
+            $table->foreign('book_id', 'fk_loan_book_book')->references('id')->on('books')->onDelete('restrict')->onUpdate('restrict');
+            $table->date('date');
+            $table->string('loaned_to', 100);
+            $table->boolean('status');
+            $table->date('return_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_book');
+    }
+}
